5f884cc8a588b206b39cba86ee19980be02bd004
mnifmariem <mnifmariem8@gmail.com>
2023-07-18 23:31:08 +0100

    Authentification

---
+++ myApp/src/app/authentification/authentification.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authentification',
+  templateUrl: './authentification.page.html',
+  styleUrls: ['./authentification.page.scss',
+  '../app.component.scss'],
+})
+export class AuthentificationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
