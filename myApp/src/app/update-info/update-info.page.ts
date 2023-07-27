9842676657bb33556f5364a9be2e10348284217e
zghalnour <nourhoudazghal@gmail.com>
2023-07-27 12:42:34 +0100

    update-info

--- myApp/src/app/update-info/update-info.page.ts
+++ myApp/src/app/update-info/update-info.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-update-info',
   templateUrl: './update-info.page.html',
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateInfoPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
+  async onUpdateInfoClick() {
+    const alert = await this.alertController.create({
+      header: 'Update Info',
+      message: 'Your information has been updated.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
