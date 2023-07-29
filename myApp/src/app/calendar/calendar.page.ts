8402ac0a6dac9766db898d629db5d5309e28e294
Eya Maazoun <98493167+eyamaazoun@users.noreply.github.com>
2023-07-29 23:54:10 +0100

    calender

--- myApp/src/app/calendar/calendar.page.ts
+++ myApp/src/app/calendar/calendar.page.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
 
+  today!: Date;
+  days!: any[]; // Replace 'any' with a specific type if available
+  events!: any[]; // Replace 'any' with a specific type if available
+
   constructor() { }
 
   ngOnInit() {
+    // Initialize the 'today' variable with the current date
+    this.today = new Date();
+
+    // Initialize the 'days' variable with an array of day objects
+    // You need to define the 'days' array content according to your requirements
+    this.days = [
+      { date: new Date(), label: 'Day 1' },
+      { date: new Date(), label: 'Day 2' },
+      // Add more day objects as needed
+    ];
+
+    // Initialize the 'events' variable with an array of event objects
+    // You need to define the 'events' array content according to your requirements
+    this.events = [
+      { title: 'Event 1', start: new Date(), end: new Date() },
+      { title: 'Event 2', start: new Date(), end: new Date() },
+      // Add more event objects as needed
+    ];
   }
 
+  onDayButtonClick(date: Date) {
+    // Implement the functionality for when a day button is clicked
+    console.log('Day button clicked:', date);
+  }
+
+  onEventClick(event: any) {
+    // Implement the functionality for when an event is clicked
+    console.log('Event clicked:', event);
+  }
 }
