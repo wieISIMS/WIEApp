235afd8f80469c4459c7304b193097b5a49ad308
Eya Maazoun <98493167+eyamaazoun@users.noreply.github.com>
2023-07-31 20:36:36 +0100

    login page

--- myApp/src/app/login/login.page.ts
+++ myApp/src/app/login/login.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss', '../app.component.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  emailmsg!: string;
+  passwordmsg!: string;
+  @ViewChild('emailInput', { static: false }) emailInput!: ElementRef;
+  @ViewChild('passwordInput', { static: false }) passwordInput!: ElementRef;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+  login() {
+    const emailValue = this.emailInput.nativeElement.value;
+    const passwordValue = this.passwordInput.nativeElement.value;
+    console.log(emailValue);
+    this.emailmsg = '';
+    this.passwordmsg = '';
 
+    // Email validation
+    const emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+    if (!emailPattern.test(emailValue) || emailValue === '') {
+      this.emailmsg = 'Please provide a properly formatted email address';
+    }
+
+    if (passwordValue === '') {
+      this.passwordmsg = 'Please provide a password';
+    }
+
+    //if (passwordValue !== 'password') {this.passwordmsg = 'Please provide a password';}
+    else{
+    //login logic
+    this.router.navigate(['/tablinks']);
 }
+}}
