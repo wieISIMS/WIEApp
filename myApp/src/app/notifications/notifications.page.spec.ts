0a209cdc5d9825172165465ca7d5d50e01135f52
zghalnour <nourhoudazghal@gmail.com>
2023-08-02 20:53:17 +0100

    notifications v2

--- myApp/src/app/notifications/notifications.page.spec.ts
+++ myApp/src/app/notifications/notifications.page.spec.ts
@@ -1,10 +1,9 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { NotificationsPage } from './notifications.page';
-import { Component } from '@angular/core';
 describe('NotificationsPage', () => {
   let component: NotificationsPage;
   let fixture: ComponentFixture<NotificationsPage>;
-
+  
   beforeEach(async(() => {
     fixture = TestBed.createComponent(NotificationsPage);
     component = fixture.componentInstance;
@@ -14,4 +13,13 @@ describe('NotificationsPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+
+  
 });
+
+
+
+
+
