47e9354899fac06becf6536427b56d7516363ca3
amel-BenAmeur <benameuramel19@gmail.com>
2023-08-06 20:22:07 +0200

    login and sign up

--- myApp/src/app/sign2/sign2.page.ts
+++ myApp/src/app/sign2/sign2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router' ;
 
 @Component({
   selector: 'app-sign2',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign2.page.scss'],
 })
 export class Sign2Page implements OnInit {
+  phoneNumber: string='';
+  userName: string='';
+  errormessage: string ='';
+  
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  isValidPhoneNumber(phoneNumber: string): boolean {
+    const phonePattern = /^[1-9]\d{1}[0-9]\d{1}[0-9]\d{4}$/;
+    return phonePattern.test(phoneNumber);
+  }
+
+  isValidUsername(userName: string): boolean {
+    const usernamePattern = /^[a-zA-Z0-9_]+$/;
+    return usernamePattern.test(userName);
+  }
+  
+  confirm(){
+   if (!this.isValidPhoneNumber(this.phoneNumber)) {
+      console.log('Invalid phone number.');
+      //this.errormessage='Invalid phone number.';
+      return;
+    }
+    
+
+     // Validate username
+     if (!this.isValidUsername(this.userName)) {
+      console.log('Invalid username.');
+      return;
+    }
+    
+    //phone number and username are valid, go to login
+    this.router.navigate(['/login']);
+  }
+  showError(): string{
+    if(!this.phoneNumber){
+      return "Phone number is required!";
+    }
+    else if(!this.userName){
+      return "User name is required!";
+    }
+    else if(this.phoneNumber && !this.isValidPhoneNumber(this.phoneNumber)){
+      return "Invalid phone number!";
+    }
+    else if(this.userName && !this.isValidUsername(this.userName)){
+      return "Invalid username!";
+    }    
+    return ' ';
+  }
+  
 
   ngOnInit() {
   }
-
+  
 }
+
+
