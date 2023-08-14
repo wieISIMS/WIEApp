9cdaba7eb575c881f48dce1990816584236fccb7
Ons turki <onsturki3@gmail.com>
2023-08-14 18:52:58 +0100

    OurClub + getLastEvent + EventDetails

--- WeConnectISIMS/serviceWeConnect/urls.py
+++ WeConnectISIMS/serviceWeConnect/urls.py
@@ -5,6 +5,10 @@ from django.conf import settings
 from . import views
 urlpatterns = [
     path('loginClub/', views.authenticateClub, name='loginClub'),
-    path('createClub/',views.inscriptionClub, name='createClub')
+    path('createClub/',views.inscriptionClub, name='createClub'),
+    path('clubs/', views.OurClubs, name='OurClubs'),
+    path('LastEvents/', views.getLastEvents, name='LastEvents'),
+     path('EventDetails/', views.getEventDetails, name='EventDetails')
+    
     ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
