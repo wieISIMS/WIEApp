697ad22bbc64831b903351fdcdf1ac9cc3479f2f
Eya Maazoun <98493167+eyamaazoun@users.noreply.github.com>
2023-08-20 17:25:57 +0100

    notif & event

--- myApp/src/app/notifications/notifications.page.ts
+++ myApp/src/app/notifications/notifications.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface notif {
+  idEvent:any;
+  image: any;
+  club:any;
+  title:any;
+  time:any
+} 
 
 @Component({
   selector: 'app-notifications',
@@ -6,12 +15,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
-  //notificationsList: any[] = []; 
-  notificationsList: any[] = [1,2,3]; 
-
-  constructor() { }
+  //notificationsList: notif[] = []; 
+  notificationsList: notif[] = [
+    {
+      idEvent:1,
+    image: 'assets/img/wie.png',
+    club: 'IEEE WIE Affinity Group',
+    title:'New event',
+    time: '45 minutes ago',
+  },
+  {
+    idEvent:2,
+    image: 'assets/microsoft.jpg',
+    club: 'Microsoft ISIMS',
+    title:'New event',
+    time: '2 hours ago',}
+  ]; 
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  goToEvent(group:any){
+    this.router.navigate(['/event'], { state: { group: group } });
+  }
 }
